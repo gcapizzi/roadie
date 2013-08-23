01057602da495a1fd6d1064471df5a3f3996bc92
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-08-23 20:58:10 +0200

    Move integration specs inside Roadie module

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,55 +3,57 @@ require 'rack/test'
 
 require './lib/roadie'
 
-describe Roadie::Router do
-  include Rack::Test::Methods
+module Roadie
+  describe Router do
+    include Rack::Test::Methods
 
-  let(:app) do
-    router = Roadie::Router.new
-    router << Roadie::Route.new(:foo, Roadie::Matcher.new('GET', '/foo'), lambda { |env| [200, {}, ['foo']] })
-    router << Roadie::Route.new(:woot, Roadie::Matcher.new('POST', %r{/w(o+)t}), lambda { |env| [200, {}, ['woot']] })
-    router << Roadie::Route.new(:p_verb, Roadie::Matcher.new(/P(.+)/, '/p-verb'), lambda { |env| [200, {}, ['p-verb']] })
+    let(:app) do
+      router = Router.new
+      router << Route.new(:foo, Matcher.new('GET', '/foo'), lambda { |env| [200, {}, ['foo']] })
+      router << Route.new(:woot, Matcher.new('POST', %r{/w(o+)t}), lambda { |env| [200, {}, ['woot']] })
+      router << Route.new(:p_verb, Matcher.new(/P(.+)/, '/p-verb'), lambda { |env| [200, {}, ['p-verb']] })
 
-    router << Roadie::Route.new(:resource, Roadie::Matcher.new(/(?<verb>.+)/, %r{/resource/(?<id>.+)/?}), lambda { |env|
-      [200, {}, ["ID: #{env['rack.routing_args']['id']}, Verb: #{env['rack.routing_args']['verb']}"]]
-    })
+      router << Route.new(:resource, Matcher.new(/(?<verb>.+)/, %r{/resource/(?<id>.+)/?}), lambda { |env|
+        [200, {}, ["ID: #{env['rack.routing_args']['id']}, Verb: #{env['rack.routing_args']['verb']}"]]
+      })
 
-    router
-  end
+      router
+    end
 
-  it 'returns 404 by default' do
-    get '/'
-    expect(last_response.status).to eq(404)
-  end
+    it 'returns 404 by default' do
+      get '/'
+      expect(last_response.status).to eq(404)
+    end
 
-  it 'matches a simple url' do
-    get '/foo'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to eq('foo')
-  end
+    it 'matches a simple url' do
+      get '/foo'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq('foo')
+    end
 
-  it 'matches an url with a regex' do
-    post '/woooooot'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to eq('woot')
-  end
+    it 'matches an url with a regex' do
+      post '/woooooot'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq('woot')
+    end
 
-  it 'matches a verb with a regex' do
-    post '/p-verb'
-    expect(last_response.status).to eq(200)
+    it 'matches a verb with a regex' do
+      post '/p-verb'
+      expect(last_response.status).to eq(200)
 
-    put '/p-verb'
-    expect(last_response.status).to eq(200)
+      put '/p-verb'
+      expect(last_response.status).to eq(200)
 
-    patch '/p-verb'
-    expect(last_response.status).to eq(200)
+      patch '/p-verb'
+      expect(last_response.status).to eq(200)
 
-    get '/p-verb'
-    expect(last_response.status).to eq(404)
-  end
+      get '/p-verb'
+      expect(last_response.status).to eq(404)
+    end
 
-  it 'stores named matches in roadie.params' do
-    put '/resource/123'
-    expect(last_response.body).to eq('ID: 123, Verb: PUT')
+    it 'stores named matches in roadie.params' do
+      put '/resource/123'
+      expect(last_response.body).to eq('ID: 123, Verb: PUT')
+    end
   end
 end
