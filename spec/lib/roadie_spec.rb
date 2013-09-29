0af8a94bc806fefe5cfdb55f0bc5c0c4be78da0b
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-09-29 19:02:39 +0200

    Add specs for Match

--- spec/lib/roadie_spec.rb
+++ spec/lib/roadie_spec.rb
@@ -163,4 +163,19 @@ module Roadie
     end
   end
 
+  describe Match do
+    it 'is not ok by default' do
+      expect(Match.new.ok?).to be_false
+    end
+
+    describe '#initialize' do
+      it 'sets the ok flag and the params' do
+        params = { foo: 'bar' }
+        match = Match.new(true, params)
+
+        expect(match).to be_ok
+        expect(match.params).to eq(params)
+      end
+    end
+  end
 end
