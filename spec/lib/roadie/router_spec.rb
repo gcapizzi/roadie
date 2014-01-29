43edd3bbb66dfc7e6e78d51d92e2a84ab14311d2
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-01-29 10:44:04 +0100

    Specs refactoring

--- spec/lib/roadie/router_spec.rb
+++ spec/lib/roadie/router_spec.rb
@@ -5,14 +5,21 @@ module Roadie
   describe Router do
     let(:ok_resp) { [200, {}, ['ok']] }
     let(:matching_route) { double(Route, call: ok_resp) }
-    let(:not_matching_route) { double(Route, call: [404, { 'X-Cascade' => 'pass' }, []]) }
+    let(:pass_resp) { [404, { 'X-Cascade' => 'pass' }, []] }
+    let(:not_matching_route) { double(Route, call: pass_resp) }
     let(:env) { {} }
+
+    subject { Router.new(routes) }
 
     describe '#call' do
       context 'when a route matches' do
         let(:other_matching_route) { double(Route, call: ok_resp) }
-
-        subject { Router.new [not_matching_route, matching_route, not_matching_route, other_matching_route] }
+        let(:routes) do
+          [not_matching_route,
+           matching_route,
+           not_matching_route,
+           other_matching_route]
+        end
 
         it 'stops trying and returns the route response' do
           other_matching_route.should_not_receive(:call)
@@ -22,8 +29,11 @@ module Roadie
         context 'when a matching route replies with X-Cascade => pass' do
           let(:ok_pass_resp) { [200, { 'X-Cascade' => 'pass' }, ['ok and pass']] }
           let(:matching_and_passing_route) { double(Route, call: ok_pass_resp) }
-
-          subject { Router.new [not_matching_route, matching_and_passing_route, matching_route] }
+          let(:routes) do
+            [not_matching_route,
+             matching_and_passing_route,
+             matching_route]
+          end
 
           it 'keeps trying other routes' do
             expect(subject.call(env)).to eq(ok_resp)
@@ -33,8 +43,11 @@ module Roadie
         context 'when a route uses X-Cascade with the wrong value' do
           let(:wrong_pass_resp) { [200, { 'X-Cascade' => 'wrong' }, ['you must use the "pass" value']] }
           let(:matching_and_passing_wrong_route) { double(Route, call: wrong_pass_resp) }
-
-          subject { Router.new [not_matching_route, matching_and_passing_wrong_route, matching_route] }
+          let(:routes) do
+            [not_matching_route,
+             matching_and_passing_wrong_route,
+             matching_route]
+          end
 
           it 'ignores the X-Cascade directive' do
             response = subject.call(env)
@@ -45,7 +58,6 @@ module Roadie
 
       context 'when no route matches' do
         let(:routes) { [not_matching_route] * 3 }
-        subject { Router.new(routes) }
 
         context 'when no default route is set' do
           it 'returns a 404 Not Found with X-Cascade => pass' do
@@ -73,13 +85,12 @@ module Roadie
     describe '#url_for' do
       let(:foo_route) { double(Route, name: :foo) }
       let(:bar_route) { double(Route, name: :bar) }
+      let(:routes) { [foo_route, bar_route] }
 
       before do
         foo_route.stub(:expand_url).with(id: '123').and_return('/foo/123')
         bar_route.stub(:expand_url).with(id: '456').and_return('/bar/456')
       end
-
-      subject { Router.new [foo_route, bar_route] }
 
       it 'expands a route URL' do
         expect(subject.url_for(:foo, id: '123')).to eq('/foo/123')
