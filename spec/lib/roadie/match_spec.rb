09731bbfe6385c5cc820f685fd871c25b1f1cb2f
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-02-13 23:31:18 +0100

    Improve Match spec

--- spec/lib/roadie/match_spec.rb
+++ spec/lib/roadie/match_spec.rb
@@ -3,17 +3,33 @@ require 'roadie/match'
 
 module Roadie
   describe Match do
-    it 'is not ok by default' do
-      expect(Match.new.ok?).to be(false)
-    end
+    let(:params) { { foo: 'bar' } }
 
     describe '#initialize' do
       it 'sets the ok flag and the params' do
-        params = { foo: 'bar' }
         match = Match.new(true, params)
 
         expect(match).to be_ok
         expect(match.params).to eq(params)
+      end
+
+      it 'creates a failed match by default' do
+        expect(Match.new).not_to be_ok
+      end
+    end
+
+    describe '.ok' do
+      it 'creates a successful match' do
+        match = Match.ok(params)
+
+        expect(match).to be_ok
+        expect(match.params).to eq(params)
+      end
+    end
+
+    describe '.fail' do
+      it 'creates a failed match' do
+        expect(Match.fail).not_to be_ok
       end
     end
   end
