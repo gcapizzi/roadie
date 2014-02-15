68d25a6e0df8c8466032c89eba8f4697bb6693e5
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-02-15 16:30:52 +0100

    Builder spec cleanup

--- spec/lib/roadie/builder_spec.rb
+++ spec/lib/roadie/builder_spec.rb
@@ -16,17 +16,13 @@ module Roadie
 
     methods.each do |method|
       describe "##{method.downcase}" do
-        before do
+        it "adds a #{method} route do the router" do
           expect(router).to receive(:<<) do |route|
             expect(route.name).to eq(route_name)
             response = Rack::MockRequest.new(route).request(method, path)
             expect(response.body).to eq("#{method} response")
           end
-        end
 
-        let(:method) { method }
-
-        it "adds a #{method} route do the router" do
           method_name = method.downcase.to_sym
           handler = proc { [200, {}, "#{method} response"] }
           subject.send(method_name, route_name, path, handler)
