bae0ac87cfbba43a5d04bb7f5a1c7d5e50c80819
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-12-29 17:35:53 +0100

    Improve the Builder spec

--- spec/lib/roadie/builder_spec.rb
+++ spec/lib/roadie/builder_spec.rb
@@ -2,23 +2,24 @@ require 'spec_helper'
 require 'rack/test'
 
 require 'roadie/builder'
-require 'roadie/router'
 
 module Roadie
   RSpec.describe Builder do
     methods = %w(GET POST PUT PATCH DELETE HEAD OPTIONS LINK UNLINK)
 
-    methods.each do |method|
-      describe "##{method.downcase}" do
-        let(:router) do
-          subject.build do
-            send(method.downcase, :foo, '/foo', proc { [200, {}, method] })
+    describe '.build' do
+      let(:app) do
+        subject.build do
+          methods.each do |method|
+            send(method.downcase, :foo, '/foo', proc { [200, {}, [method]] })
           end
         end
+      end
 
-        it "adds a #{method} route do the router" do
-          response = router.call(req(method, '/foo'))
-          expect(response[2]).to eq(method)
+      it 'builds a Roadie app' do
+        methods.each do |method|
+          response = app.call(req(method, '/foo'))
+          expect(response[2].first).to eq(method)
         end
       end
     end
